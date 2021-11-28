b71d16822937e193fc82f6c33655e0e6202a958d
Armands Vilciņš <armandsvilcins3@Inbox.lv>
2021-11-28 19:36:50 +0200

    forgot to add :(

---
+++ app/Http/Controllers/SupplierController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use App\Services\StoreSupplierService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class SupplierController extends Controller
+{
+    public function store(Request $request, StoreSupplierService $service): JsonResponse
+    {
+        $this->validate($request, [
+            'delivery_code' => 'required',
+            'title' => 'required',
+            'address' => 'required',
+            'city' => 'required',
+            'phone_number' => 'required'
+        ]);
+
+        $service->execute($request);
+
+        return response()->json([
+            'message' => 'Supplier has been added successfully'
+        ], 201);
+    }
+}
